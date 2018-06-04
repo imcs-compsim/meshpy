a7aa060c5d3daa87e8a1287c3103ea19d5738012
Ivo Steinbrecher <ivo.steinbrecher@unibw.de>
2018-06-04 13:21:29 +0200

    Added config file.

---
+++ meshpy/conf.py
@@ -0,0 +1,15 @@
+
+
+
+
+
+class MeshPy(object):
+    """
+    A global object that stores options for the whole meshpy application.
+    """
+    
+    def __init__(self, *args, **kwargs):
+        print(1)
+
+
+meshpy_conf = MeshPy()
