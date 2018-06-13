3d89f001519485e3f6ff511b4edd1e728fe14f2b
Ivo Steinbrecher <ivo.steinbrecher@unibw.de>
2018-06-13 11:08:01 +0200

    Cleanup function

--- meshpy/function.py
+++ meshpy/function.py
@@ -1,16 +1,23 @@
+# -*- coding: utf-8 -*-
+"""
+This module implements a basic class to manage functions in the baci input file.
+"""
+
 # import meshpy modules
 from . import BaseMeshItem
 
 
 class Function(BaseMeshItem):
-    """ Holds information for a function. """
-    
+    """Holds information for a function."""
+
     def __init__(self, data):
         BaseMeshItem.__init__(self, data=data, is_dat=False)
-    
-    
+
     def __str__(self):
-        """ Check if the function has a global index. """
+        """
+        Return the global index for this function. This is usually used then the
+        function is called with the str.format() function.
+        """
         if self.n_global:
             return str(self.n_global)
         else:
