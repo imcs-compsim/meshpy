61c42a1d4bfa12fda9db0fb85dde8f37e0e4f4c8
Ivo Steinbrecher <ivo.steinbrecher@unibw.de>
2018-07-05 14:46:12 +0200

    Version 0.0.3

--- meshpy/conf.py
+++ meshpy/conf.py
@@ -13,7 +13,7 @@ class MeshPy(object):
     def __init__(self):
 
         # Version information
-        self.version = '0.0.2'
+        self.version = '0.0.3'
         self.git_sha = None
         self.git_date = None
         
