eccbdb16e74f08cc315eba5c2c279fc96b3e7bf0
Ivo Steinbrecher <ivo.steinbrecher@unibw.de>
2018-07-14 20:59:32 +0200

    Cython module can now be compield with script.

--- meshpy/find_close_nodes_setup.py
+++ meshpy/find_close_nodes_setup.py
@@ -4,11 +4,20 @@ This script is used to compile the cyton code.
 python3 find_close_nodes_setup.py build_ext --inplace
 """
 
+# Python packages.
+import numpy as np
+import sys
+
+# Cython stuff.
 from distutils.core import setup
 from Cython.Build import cythonize
 
-import numpy as np
+# Check if the script is called with arguments.
+if len(sys.argv) == 1:
+    # No arguments are given, set them.
+    sys.argv = [__file__, 'build_ext', '--inplace']
 
+# Compile cython module.
 setup(
     ext_modules=cythonize('find_close_nodes.pyx', annotate=True),
     include_dirs=[np.get_include()]
