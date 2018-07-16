Added relative rotations to the roation class. Added inv() to rotation class. Added test case for the two functions above. Fixed a bug when checking if the roation angle is 0. Rotation class can now be initialized with a 0 vector, if the angle is also 0.

meshpy/rotation.py
@@ -32,10 +32,10 @@ class Rotation(object):
             vector = args[0]
             phi = args[1]
             norm = np.linalg.norm(vector)
-            if norm < mpy.eps_quaternion:
-                raise ValueError('The rotation axis can not be a zero vector!')
             if np.abs(phi) < mpy.eps_quaternion:
                 self.q[0] = 1
+            elif norm < mpy.eps_quaternion:
+                raise ValueError('The rotation axis can not be a zero vector!')
             else:
                 self.q[0] = np.cos(0.5 * phi)
                 self.q[1:] = np.sin(0.5 * phi) * \
@@ -115,11 +115,20 @@ class Rotation(object):
         norm = np.linalg.norm(self.q[1:])
         phi = 2 * np.arctan2(norm, self.q[0])
 
-        # Check if phi is 0
-        if phi < mpy.eps_quaternion:
+        # Check if effective rotation angle is 0
+        if np.abs(np.sin(phi / 2)) < mpy.eps_quaternion:
             return np.zeros(3)
         else:
             return phi * self.q[1:] / norm
+
+    def inv(self):
+        """
+        Return the inverse of this rotation.
+        """
+
+        tmp_quaternion = self.q.copy()
+        tmp_quaternion[0] *= -1.
+        return Rotation(tmp_quaternion)
 
     def __mul__(self, other):
         """
@@ -183,3 +192,8 @@ class Rotation(object):
             str(self.q[0]),
             str(self.q[1:])
             )
+
+
+def get_relative_rotation(rotation1, rotation2):
+    """Return the rotation from rotation1 to rotation2."""
+    return rotation2 * rotation1.inv()

tests/testing.py
@@ -22,9 +22,9 @@ sys.path.insert(0, os.path.abspath(os.path.join(
     os.path.dirname(__file__), '..')))
 
 # Meshpy imports.
-from meshpy import mpy, Rotation, InputFile, InputSection, Material, Mesh, \
-    Function, Beam3rHerm2Lin3, BoundaryCondition, Node, BaseMeshItem, \
-    VTKWriter, compare_xml
+from meshpy import mpy, Rotation, get_relative_rotation, InputFile, \
+    InputSection, Material, Mesh, Function, Beam3rHerm2Lin3, \
+    BoundaryCondition, Node, BaseMeshItem, VTKWriter, compare_xml
 
 
 # Define the testing paths.
@@ -150,6 +150,30 @@ class TestRotation(unittest.TestCase):
 
         q = np.array([0.97862427, -0.0884585, -0.16730294, -0.0804945])
         self.assertTrue(Rotation(q) == Rotation(-q))
+
+    def test_inverse_rotation(self):
+        """Test the inv() function for rotations."""
+
+        rot = Rotation([1, 2, 3], 2)
+
+        # Check if inverse rotation gets identity rotation. Use two different
+        # constructors for identity rotation.
+        self.assertTrue(Rotation([0, 0, 0], 0) == rot * rot.inv())
+        self.assertTrue(Rotation() == rot * rot.inv())
+
+        # Check that there is no warning or error when getting the vector for
+        # an identity rotation.
+        (rot * rot.inv()).get_roation_vector()
+
+    def test_relative_roation(self):
+        """Test the relative rotation between two rotations."""
+
+        rot1 = Rotation([1, 2, 3], 2)
+        rot2 = Rotation([0.1, -0.2, 2], np.pi / 5)
+
+        rot21 = get_relative_rotation(rot1, rot2)
+
+        self.assertTrue(rot2 == rot21 * rot1)
 
 
 def create_test_mesh(mesh):