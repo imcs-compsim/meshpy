62cf6bfee0ac0280e83235f210862a588c47db24
Ivo Steinbrecher <ivo.steinbrecher@unibw.de>
2022-11-08 11:00:19 +0100

    Update slurm script to new cluster configuration

--- meshpy/simulation_manager/batch_template.sh
+++ meshpy/simulation_manager/batch_template.sh
@@ -1,89 +1,105 @@
-#!/bin/sh -f
-#
-###############################
-# Specify your SLURM directives
-###############################
+#!/bin/bash
+##########################################
+#                                        #
+#  Specify your SLURM directives         #
+#                                        #
+##########################################
 # User's Mail:
-#SBATCH --mail-user=matthias.mayr@unibw.de
+#SBATCH --mail-user=<your_email@provider.com>
 # When to send mail?:
-#SBATCH --mail-type=BEGIN,END,FAIL
+#SBATCH --mail-type=NONE
 #
 # Job name:
-#SBATCH --job-name "{job_name}"
-#SBATCH --output=slurm-%j.out
-#SBATCH --error=slurm-%j.err
+#SBATCH --job-name={job_name}
 #
-# Define if the job should only be run on certain nodes.
-#{is_feature}SBATCH --constraint={self.feature}
+# Output file:
+#SBATCH --output=slurm-%j-%x.out
+#
+# Error file:
+#SBATCH --error=slurm-%j-%x.err
 #
 # Standard case: specify only number of cpus
-# #SBATCH --ntasks=24
+# #SBATCH --ntasks=8
 #
-# If you want to specify a certain number of nodes
-# and exactly 'ntasks-per-node' cpus on each node.
+# Walltime: (days-hours:minutes:seconds)
+#SBATCH --time={self.wall_time}
+#
+##########################################
+#                                        #
+#  Advanced SLURM settings	          #
+#                                        #
+##########################################
+#
+# If you want to specify a certain number of nodes:
 #SBATCH --nodes={self.n_nodes}
+#
+# and exactly 'ntasks-per-node' cpus on each node:
 #SBATCH --ntasks-per-node={self.n_proc}
 #
-# For hybrid mpi: e.g. 2 mpi processes each with
-# 4 openmp threads
-# #SBATCH --ntasks=2
-# #SBATCH --cpus-per-task=4
-#
-# Allocate full node and block for other jobs
+# Allocate full node and block for other jobs:
 #{is_exclusive}SBATCH --exclusive
 #
-# Walltime:
-#SBATCH --time={self.wall_time}
+# Request specific hardware features:
+#{is_feature}SBATCH --constraint={self.feature}
+#
 ###########################################
 
-# Store calling directroy
-CWD=$(pwd)
-
-# Setup shell environment
+# Setup shell environment and start from home dir
 echo $HOME
 cd $HOME
 source /etc/profile.d/modules.sh
 source /home/opt/cluster_tools/core/load_baci_environment.sh
 
-# Go back to calling directroy
-cd  $CWD
+module list
+##########################################
+#                                        #
+#  Specify the paths                     #
+#                                        #
+##########################################
 
-########################
-# GENERAL SPECIFICATIONS
-########################
-BUILD_DIR="$BACI_BUILD_DIR"
 RUN_BACI="ON"
-RUN_ENSIGHT_FILTER="OFF"
+BACI_BUILD_DIR="$BACI_BUILD_DIR"
+EXE=$BACI_BUILD_DIR/baci-release
 
-#####################
-# INPUT SPECIFICATION
-#####################
 INPUT="$SIMULATIONS_DIR/{input_file_name}"
-
-######################
-# OUTPUT SPECIFICATION
-######################
-OUTPUT_PREFIX="{self.output_prefix}"
 BACI_OUTPUT_DIR="$SIMULATIONS_DIR"
+OUTPUT_PREFIX="{self.output_prefix}"
 
-#######################
-# RESTART SPECIFICATION
-#######################
-RESTART_FROM_STEP={self.restart_step}                 # <= specify your restart step
-RESTART_FROM_DIR="{self.restart_dir}"
-RESTART_FROM_PREFIX="{self.restart_from_prefix}" # <= specify the result prefix from which restart read
+
+##########################################
+#                                        #
+#  Postprocessing                        #
+#                                        #
+##########################################
+
+RUN_ENSIGHT_FILTER="OFF"
+ENSIGHT_OUTPUT_DIR=""
+ENSIGHT_OPTIONS=""
+
+
+##########################################
+#                                        #
+#  RESTART SPECIFICATION                 #
+#                                        #
+##########################################
+
+RESTART_FROM_STEP={self.restart_step}            # specify the restart step here and in .datfile
+RESTART_FROM_DIR="{self.restart_dir}"            # same as output
+RESTART_FROM_PREFIX="{self.restart_from_prefix}" # prefix typically xxx
 
 #################################################################
 # BEGIN ############### DO NOT TOUCH ME #########################
 #################################################################
+
 # execute program
-source /home/opt/cluster_tools/core/baci_job_core
-trap 'early; stageout' 2 9 15 18
-dochecks
-stagein
-runprogram
-stageout
-show
+source /home/opt/cluster_tools/core/charon_job_core
+trap 'EarlyTermination; StageOut' 2 9 15 18
+DoChecks
+StageIn
+RunProgram
+wait
+StageOut
+# show
 # END ################## DO NOT TOUCH ME #########################
 echo
 echo "Job finished with exit code $? at: `date`"
