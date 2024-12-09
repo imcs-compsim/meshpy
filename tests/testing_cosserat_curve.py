06da986861ce081476f4960404fd8335ef28583b
Ivo Steinbrecher <ivo.steinbrecher@unibw.de>
2024-12-09 21:16:45 +0100

    Add some stuff

--- tests/testing_cosserat_curve.py
+++ tests/testing_cosserat_curve.py
@@ -1,4 +1,33 @@
 # -*- coding: utf-8 -*-
+# -----------------------------------------------------------------------------
+# MeshPy: A beam finite element input generator
+#
+# MIT License
+#
+# Copyright (c) 2018-2024
+#     Ivo Steinbrecher
+#     Institute for Mathematics and Computer-Based Simulation
+#     Universitaet der Bundeswehr Muenchen
+#     https://www.unibw.de/imcs-en
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in
+# all copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+# -----------------------------------------------------------------------------
 """
 Test the functionality of the Cosserat curve module
 """
@@ -27,18 +56,15 @@ def load_cosserat_curve_from_file():
 
 
 def test_translate_and_rotate():
-    """Test that a curve can be loaded and rotated and transformed"""
+    """Test that a curve can be loaded, rotated and transformed"""
 
-    # Load the curve
     curve = load_cosserat_curve_from_file()
 
     # Translate the curve so that the start is at the origin
     curve.translate(-curve.centerline_interpolation(5.0))
 
     # Rotate the curve around its center point
-    tmp = curve.get_centerline_position_and_rotation(0.0)
-    pos_1 = tmp[0]
-    q_1 = tmp[1]
+    pos_1, q_1 = curve.get_centerline_position_and_rotation(0.0)
     curve.rotate(q_1, origin=pos_1)
 
     # Get the points and rotations at certain points
@@ -122,4 +148,5 @@ def test_warp_mesh():
         os.path.join(testing_input, "test_cosserat_curve_balloon_mesh_warp.dat")
     ) as f:
         data = f.read()
+    # Todo: Once we switched to pytest, lets put the general compare results function here
     assert compare_string_tolerance(data, input_file_string, rtol=1e-10)
