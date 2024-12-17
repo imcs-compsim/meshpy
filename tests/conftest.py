66a9dfe003afe06cdcbdf0c42606b4e11d6cc857
David Rudlstorfer <david.rudlstorfer@tum.de>
2024-12-17 10:31:16 +0100

    Add first version of pytest infrastructure
    Includes first fixtures and tagging approach
    Also update first pytest test (cosserat curve) to use new approaches

---
+++ tests/conftest.py
@@ -0,0 +1,137 @@
+# -*- coding: utf-8 -*-
+# -----------------------------------------------------------------------------
+# MeshPy: A beam finite element input generator
+#
+# MIT License
+#
+# Copyright (c) 2018-2024
+#     Ivo Steinbrecher
+#     Institute for Mathematics and Computer-Based Simulation
+#     Universitaet der Bundeswehr Muenchen
+#     https://www.unibw.de/imcs-en
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in
+# all copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+# -----------------------------------------------------------------------------
+"""Testing framework infrastructure."""
+
+import os
+from pathlib import Path
+
+import pytest
+from _pytest.config import Config
+from _pytest.config.argparsing import Parser
+
+
+def pytest_addoption(parser: Parser) -> None:
+    """Add custom command line options to pytest.
+
+    Args:
+        parser: Pytest parser
+    """
+
+    parser.addoption(
+        "--4C_ArborX",
+        action="store_true",
+        default=False,
+        help="Execute 4C and ArborX based tests.",
+    )
+
+    parser.addoption(
+        "--CubitPy",
+        action="store_true",
+        default=False,
+        help="Execute CubitPy based tests.",
+    )
+
+    parser.addoption(
+        "--performance",
+        action="store_true",
+        default=False,
+        help="Execute performance tests.",
+    )
+
+
+def pytest_collection_modifyitems(config: Config, items: list) -> None:
+    """Filter tests based on their markers and provided command line options.
+
+    Currently configured options:
+        `pytest`: execute tests with no markers
+        `pytest --4C_ArborX`: execute tests with the `fourc_arborx` marker
+        `pytest --CubitPy`: execute tests with the `cubitpy` marker
+        `pytest --performance`: execute tests with the `performance` marker
+
+    Args:
+        config: Pytest config
+        items: Pytest list of tests
+    """
+
+    selected_tests = []
+
+    # loop over all collected tests
+    for item in items:
+        # Get all set markers for current test (e.g. `fourc_arborx`, `cubitpy`, `performance`)
+        markers = [marker.name for marker in item.iter_markers()]
+
+        if config.getoption("--4C_ArborX") and "fourc_arborx" in markers:
+            selected_tests.append(item)
+
+        if config.getoption("--CubitPy") and "cubitpy" in markers:
+            selected_tests.append(item)
+
+        if config.getoption("--performance") and "performance" in markers:
+            selected_tests.append(item)
+
+        if not markers:
+            selected_tests.append(item)
+
+    deselected_tests = list(set(items) - set(selected_tests))
+
+    items[:] = selected_tests
+    config.hook.pytest_deselected(items=deselected_tests)
+
+
+@pytest.fixture(scope="session")
+def reference_file_directory() -> Path:
+    """Provide the path to the reference file directory.
+
+    Returns:
+        Path: A Path object representing the full path to the reference file directory.
+    """
+
+    testing_path = os.path.abspath(os.path.dirname(__file__))
+    return Path(testing_path) / "reference-files"
+
+
+@pytest.fixture(scope="function")
+def current_test_name(request: pytest.FixtureRequest) -> str:
+    """Return the name of the current pytest test.
+
+    Args:
+        request: The pytest request object.
+
+    Returns:
+        str: The name of the current pytest test.
+    """
+
+    return request.node.name
+
+
+@pytest.fixture
+def compare_result_file(reference, result, rtol, atol):
+    raise NotImplementedError
