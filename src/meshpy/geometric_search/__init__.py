b3ad324745b59e6c16d72e73168cbe0ae564a1ea
David Rudlstorfer <david.rudlstorfer@tum.de>
2025-02-27 15:10:32 +0100

    Review comments

--- src/meshpy/geometric_search/__init__.py
+++ src/meshpy/geometric_search/__init__.py
@@ -23,24 +23,24 @@
 
 This module contains functionality to find points close to each other in
 a point cloud. Currently, three different implementations for the actual
-search algorithm are available (depending on your install/setup):
+search algorithm are available (depending on your installation/setup):
 
 - `brute_force_cython`: A brute force algorithm implemented in Cython,
-  scales with `O(n^2)`, but for a small number of points (n<200) this is
-  the fastest algorithm since compared to the others it does not have
-  any setup costs.
+  scales with $\mathcal{O}(n^2)$, but for a small number of points
+  ($n<200$) this is the fastest algorithm since compared to the others
+  it does not have any setup costs.
 
 - `kd_tree_scipy`: Uses a BVH implementation provided by scipy. This
-  scales with `O(n*log(n))`.
+  scales with $\mathcal{O}(n\ \log{n})$.
 
 - `boundary_volume_hierarchy_arborx` : Uses a BVH implementation
-  provided by ArborX. This also scales with `O(n*log(n))` but due to a
-  more optimised implementation is a few times faster than the scipy
-  implementation.
+  provided by ArborX. This also scales with $\mathcal{O}(n\ \log{n})$
+  but due to a more optimised implementation is a few times faster
+  than the scipy implementation.
 
-The algorithm chooses automatically fastest (available) algorithm for
-the given coordinate array.
+The `find_close_points` function automatically chooses the fastest
+(available) implementation for the given point array.
 
-Consult the `readme.md` regarding install and testing options for
+Consult the `README.md` regarding install and testing options for
 different implementations.
 """
