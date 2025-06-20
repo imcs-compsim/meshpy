7daa01a4bf387a800f923950c1255789c01f722e
Ivo Steinbrecher <ivo.steinbrecher@unibw.de>
2025-06-20 09:42:55 +0200

    Add 2D and 3D test case (2D case is modified to shell example)

--- tests/test_nurbs.py
+++ tests/test_nurbs.py
@@ -22,11 +22,15 @@
 """This script is used to test the mesh creation functions for NURBS."""
 
 import numpy as np
+import splinepy
 
 from meshpy.core.mesh import Mesh
 from meshpy.core.rotation import Rotation
-from meshpy.four_c.material import MaterialStVenantKirchhoff
-from meshpy.mesh_creation_functions.nurbs_generic import add_geomdl_nurbs_to_mesh
+from meshpy.four_c.material import MaterialSolid, MaterialStVenantKirchhoff
+from meshpy.mesh_creation_functions.nurbs_generic import (
+    add_geomdl_nurbs_to_mesh,
+    add_splinepy_nurbs_to_mesh,
+)
 from meshpy.mesh_creation_functions.nurbs_geometries import (
     create_nurbs_brick,
     create_nurbs_cylindrical_shell_sector,
@@ -85,17 +89,13 @@ def test_nurbs_flat_plate_2d(
     mesh = Mesh()
 
     # Add material
-    mat = MaterialStVenantKirchhoff(youngs_modulus=710, nu=0.19, density=5.3e-7)
+    mat = MaterialSolid(
+        material_string="MAT_Kirchhoff_Love_shell",
+        data={"YOUNG_MODULUS": 10.0, "POISSON_RATIO": 0.3, "THICKNESS": 0.05},
+    )
 
     # Create patch set
-    element_description = {
-        "KINEM": "linear",
-        "EAS": "none",
-        "THICK": 1.0,
-        "STRESS_STRAIN": "plane_strain",
-        "GP": [3, 3],
-    }
-
+    element_description = {"type": "SHELL_KIRCHHOFF_LOVE_NURBS", "GP": [3, 3]}
     patch_set = add_geomdl_nurbs_to_mesh(
         mesh, surf_obj, material=mat, data=element_description
     )
@@ -106,10 +106,46 @@ def test_nurbs_flat_plate_2d(
     assert_results_equal(get_corresponding_reference_file_path(), mesh)
 
 
+def test_nurbs_flat_plate_2d_splinepy(
+    assert_results_equal, get_corresponding_reference_file_path
+):
+    """Test the creation of a two dimensional flat plate with splinepy."""
+
+    # Create the surface of a flat plate
+    n_ele_u = 2
+    n_ele_v = 5
+    surf_obj = splinepy.helpme.create.box(0.75, 0.91).nurbs
+    surf_obj.elevate_degrees([0, 1])
+    surf_obj.insert_knots(0, np.linspace(0, 1, n_ele_u + 1))
+    surf_obj.insert_knots(1, np.linspace(0, 1, n_ele_v + 1))
+
+    control_points_3d = np.zeros([len(surf_obj.control_points), 3])
+    control_points_3d[:, :2] = surf_obj.control_points
+    surf_obj.control_points = control_points_3d - 0.5 * np.array([0.75, 0.91, 0])
+
+    # Create the shell mesh
+    mesh = Mesh()
+    mat = MaterialSolid(
+        material_string="MAT_Kirchhoff_Love_shell",
+        data={"YOUNG_MODULUS": 10.0, "POISSON_RATIO": 0.3, "THICKNESS": 0.05},
+    )
+    element_description = {"type": "SHELL_KIRCHHOFF_LOVE_NURBS", "GP": [3, 3]}
+    patch_set = add_splinepy_nurbs_to_mesh(
+        mesh, surf_obj, material=mat, data=element_description
+    )
+    mesh.add(patch_set)
+    assert_results_equal(
+        get_corresponding_reference_file_path(
+            reference_file_base_name="test_nurbs_flat_plate_2d"
+        ),
+        mesh,
+    )
+
+
 def test_nurbs_brick(assert_results_equal, get_corresponding_reference_file_path):
     """Test the creation of a brick."""
 
-    # Create the surface of a flat plate
+    # Create a brick
     vol_obj = create_nurbs_brick(1.5, 3.0, 2.4, n_ele_u=2, n_ele_v=3, n_ele_w=4)
 
     # Create mesh
@@ -129,6 +165,43 @@ def test_nurbs_brick(assert_results_equal, get_corresponding_reference_file_path
 
     # Compare with the reference file
     assert_results_equal(get_corresponding_reference_file_path(), mesh)
+
+
+def test_nurbs_brick_splinepy(
+    assert_results_equal, get_corresponding_reference_file_path
+):
+    """Test the creation of a brick with splinepy."""
+
+    # Create a brick
+    n_el_dim = [2, 3, 4]
+    box_dimensions = [1.5, 3.0, 2.4]
+    vol_obj = splinepy.helpme.create.box(*box_dimensions).nurbs
+    vol_obj.elevate_degrees([0, 1, 2])
+    for i_dim, n_el in enumerate(n_el_dim):
+        vol_obj.insert_knots(i_dim, np.linspace(0, 1, n_el + 1))
+
+    control_points_3d = np.zeros([len(vol_obj.control_points), 3])
+    control_points_3d[:, :3] = vol_obj.control_points
+    vol_obj.control_points = control_points_3d - 0.5 * np.array(box_dimensions)
+
+    # Create mesh
+    mesh = Mesh()
+
+    # Add material
+    mat = MaterialStVenantKirchhoff(youngs_modulus=710, nu=0.19, density=5.3e-7)
+
+    # Create patch set
+    patch_set = add_splinepy_nurbs_to_mesh(mesh, vol_obj, material=mat)
+
+    mesh.add(patch_set)
+
+    # Compare with the reference file
+    assert_results_equal(
+        get_corresponding_reference_file_path(
+            reference_file_base_name="test_nurbs_brick"
+        ),
+        mesh,
+    )
 
 
 def test_nurbs_rotation_nurbs_surface(
